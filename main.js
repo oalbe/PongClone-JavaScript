3d9b1deb5c212b0e45eaef4cbf1163cec9481416
Alberto <alberto.ognibene@gmail.com>
2016-09-20 09:36:07 +0200

    Move all the game logic inside the 'Game' class.
    Everything seems to work fine, though this might be a questionable way of doing thing.
    
    speeding up the loop uncontrollably.

--- main.js
+++ main.js
@@ -48,12 +48,12 @@ class Game {
     }
 
     loop(previous) {
-        requestAnimationFrame(this.loop.bind(this, previous));
-
         let current = Date.now();
         let elapsed = current - previous;
         previous = current;
         this._lag += elapsed;
+
+        requestAnimationFrame(this.loop.bind(this, previous));
 
         // Process input here
 
@@ -228,7 +228,7 @@ class Game {
         }
 
         if (this.isWinScreen) {
-            this.drawStaticScreen(this.winScreenTitle, loseWinScreenBlink);
+            this.drawStaticScreen(this.winScreenTitle, this.loseWinScreenBlink);
             return;
         }
 
@@ -257,3 +257,76 @@ class Game {
         this.canvasContext.drawCirc('white', this.ball.radius, this.ball.pos.x, this.ball.pos.y);
     } // /render()
 }
+
+// ###############################################################################################
+
+let prev = Date.now();
+
+class Coord {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+let canvas = document.getElementById('game-canvas');
+let canvasContext = canvas.getContext('2d');
+
+canvas.hCenter = canvas.width / 2;
+canvas.vCenter = canvas.height / 2;
+
+let pong = new Game(canvas, canvasContext);
+
+(function() {
+    pong.canvas.addEventListener('mousemove', function(event) {
+        if (pong.isPaused) return;
+
+        pong.racketLeft.moveToRelative(getMousePosition(event).y);
+    });
+
+    document.addEventListener('keydown', function(event) {
+        // Toggle pause
+        if (event.key === 'p') {
+            pong.isPaused = !pong.isPaused;
+            pong.audioEffects.effects.game_paused.play();
+
+            console.log('p-key down: Game paused.');
+        }
+
+        // Toggle mute
+        if (event.key === 'm') {
+            pong.soundButton.click();
+            pong.audioEffects.toggleMuteAll();
+        }
+
+        // Space pressed
+        if (event.key === ' ') {
+            if (pong.isSplashScreen) {
+                pong.isSplashScreen = false;
+            }
+
+            if (pong.isLoseScreen || pong.isWinScreen) {
+                pong.isLoseScreen = pong.isWinScreen = false;
+                pong.ball.restart();
+                pong.playerRight.setScore(0);
+                pong.playerLeft.setScore(0);
+            }
+        }
+    });
+
+    pong.canvas.addEventListener('click', function(event) {
+        let mousePos = getMousePosition(event);
+
+        // Check if the click happened inside the boundaries of the button.
+        if ((mousePos.x >= pong.soundButton.pos.x) &&
+            (mousePos.x <= (pong.soundButton.pos.x + pong.soundButton.width))) {
+            if ((mousePos.y >= pong.soundButton.pos.y) &&
+                (mousePos.y <= (pong.soundButton.pos.y + pong.soundButton.height))) {
+                pong.soundButton.click();
+                pong.audioEffects.toggleMuteAll();
+            }
+        }
+    });
+
+    pong.loop(prev);
+})();
